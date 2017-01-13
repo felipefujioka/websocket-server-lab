8e018c64f32e481e23ef783da64599f5df282012
Felipe Jun Fujioka Shida <ffujioka@topster.com.br>
2017-01-13 09:54:29 -0200

    added pubnub sdk

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,11 +15,15 @@ import java.util.ArrayList;
 
 import com.google.gson.Gson;
 
+import com.pubnub.api.*;
+import org.json.*;
+
 public class Main {
 	
 	public static int counter = 0;
 	public static SocketIOServer server;
 	public static ArrayList<Order> orders;
+	public static int currentSequenceNumber = 0;
 	
 	public static class Order {
 		public String id;
@@ -38,6 +42,14 @@ public class Main {
 		public String type;
 		public String id;
 		public String fields;
+		public int seq = 0;
+		public int size;
+		
+		public Message() {
+			this.seq = ++currentSequenceNumber;
+			this.size = Main.orders.size();
+		}
+		
 	}
 	
 	public static String getMD5(Object obj) {
@@ -58,20 +70,74 @@ public class Main {
         config.setHostname("localhost");
         config.setPort(10443);
 
+        Pubnub pubnub = new Pubnub("pub-c-ca89589b-68e4-48ee-85dd-2cc9cf01280a", "sub-c-6ded6650-d904-11e6-a478-02ee2ddab7fe");
+
+        
+        try {
+        	  pubnub.subscribe("my_channel", new Callback() {
+        	      @Override
+        	      public void connectCallback(String channel, Object message) {
+        	          pubnub.publish("my_channel", "Hello from the PubNub Java SDK", new Callback() {});
+        	      }
+        	 
+        	      @Override
+        	      public void disconnectCallback(String channel, Object message) {
+        	          System.out.println("SUBSCRIBE : DISCONNECT on channel:" + channel
+        	                     + " : " + message.getClass() + " : "
+        	                     + message.toString());
+        	      }
+        	 
+        	      public void reconnectCallback(String channel, Object message) {
+        	          System.out.println("SUBSCRIBE : RECONNECT on channel:" + channel
+        	                     + " : " + message.getClass() + " : "
+        	                     + message.toString());
+        	      }
+        	 
+        	      @Override
+        	      public void successCallback(String channel, Object message) {
+        	          System.out.println("SUBSCRIBE : " + channel + " : "
+        	                     + message.getClass() + " : " + message.toString());
+        	      }
+        	 
+        	      @Override
+        	      public void errorCallback(String channel, PubnubError error) {
+        	          System.out.println("SUBSCRIBE : ERROR on channel " + channel
+        	                     + " : " + error.toString());
+        	      }
+        	    }
+        	  );
+        	} catch (PubnubException e) {
+        	  System.out.println(e.toString());
+        	}
+        
         server = new SocketIOServer(config);
         
         server.addEventListener("subscribe", String.class, new DataListener<String>() {
             @Override
             public void onData(SocketIOClient client, String data, AckRequest ackRequest) {
             	if(data.equals("orders")) {
-            		Main.orders.forEach(order -> {
+            		
+            		// makes a snapshot of the state to send to client
+            		ArrayList<Order> copy = new ArrayList<Order>(Main.orders.size());
+            		synchronized(this) {
+	            		Main.orders.forEach(item -> {
+	            			copy.add(item);
+	            		});
+            		}
+            		
+            		copy.forEach(order -> {
             			Message msg = new Message();
             			msg.action = "added";
             			msg.type = "order";
             			msg.id = "" + order.id;
             			msg.fields = new Gson().toJson(order);
-            			client.sendEvent("update", msg);
+            			msg.seq = -1;
+            			client.sendEvent("snapshot", msg);
             		});
+            		// sends the updated current sequence number so the client can ignore old updates
+            		Message readyMsg = new Message();
+            		readyMsg.seq = -1;
+        			client.sendEvent("ready", readyMsg);
             	}
             }
         });
@@ -125,11 +191,27 @@ public class Main {
             response.type("application/json");
         });
         
+        get("/hello", (req, res) -> {
+        	
+        	Callback callback = new Callback() {
+    		  public void successCallback(String channel, Object response) {
+    		    System.out.println(response.toString());
+    		  }
+    		  public void errorCallback(String channel, PubnubError error) {
+    		    System.out.println(error.toString());
+    		  }
+    		};
+    		
+    		pubnub.publish("my_channel", "Hello from the PubNub Java SDK!" , callback);
+    		
+        	return "Hello!";
+        });
+        
         post("/orders", (req, res) -> {
         	
         	Gson gson = new Gson();
         	Order body = gson.fromJson(req.body(), Order.class);
-        	if(!orders.contains(body)){
+        	if(body.id != null && !body.id.equals("") && !orders.contains(body)){
 	        	orders.add(body);
 	        	Message msg = new Message();
 	        	msg.action = "added";
@@ -138,7 +220,7 @@ public class Main {
 	        	msg.fields = gson.toJson(body);
 	        	Main.server.getBroadcastOperations().sendEvent("update", msg);
         	}
-        	return "posted";
+        	return "{}";
         });
         
         put("/orders/:id", (req, res) -> {
@@ -152,7 +234,7 @@ public class Main {
         	msg.id = body.id;
         	msg.fields = gson.toJson(body);
         	Main.server.getBroadcastOperations().sendEvent("update", msg);
-        	return "updated";
+        	return "{}";
         });
         
         delete("/orders/:id", (req, res) -> {
@@ -177,12 +259,31 @@ public class Main {
 				// TODO Auto-generated method stub
 				while(true) {
 					try {
-						Thread.sleep(2000);
-//						Message msg = new Message();
-//			        	msg.action = "added";
-//			        	msg.type = "order";
-//			        	msg.id = "" + Math.random();
-//						Main.server.getBroadcastOperations().sendEvent("update", new Gson().toJson(msg));
+						Thread.sleep(100);
+						synchronized(this) {
+							Gson gson = new Gson();
+							if (Main.orders.size() == 0 || Math.random() > 0.5){
+								Order order = new Order();
+								order.id = ""+((int)Math.floor(Math.random() * 1000));
+								order.name = "name " + order.id;
+								order.value = ""+((int)Math.floor((Math.random()*100)));
+								Main.orders.add(order);
+								Message msg = new Message();
+					        	msg.action = "added";
+					        	msg.type = "order";
+					        	msg.id = "" + order.id;
+					        	msg.fields = gson.toJson(order);
+								Main.server.getBroadcastOperations().sendEvent("update", msg);
+							}else {
+								Order order = Main.orders.remove(0);
+								Message msg = new Message();
+					        	msg.action = "removed";
+					        	msg.type = "order";
+					        	msg.id = "" + order.id;
+					        	msg.fields = gson.toJson(order);
+								Main.server.getBroadcastOperations().sendEvent("update", msg);
+							}
+						}
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -190,9 +291,50 @@ public class Main {
 					
 				}
 			}
-		}).run();
+		}).start();
 
-//        Thread.sleep(Integer.MAX_VALUE);
+        new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(true) {
+					try {
+						Thread.sleep(50);
+						synchronized(this) {
+							Gson gson = new Gson();
+							if (Main.orders.size() == 0 || Math.random() > 0.5){
+								Order order = new Order();
+								order.id = ""+((int)Math.floor(Math.random() * 1000));
+								order.name = "name from 2 " + order.id;
+								order.value = ""+((int)Math.floor((Math.random()*100)));
+								Main.orders.add(order);
+								Message msg = new Message();
+					        	msg.action = "added";
+					        	msg.type = "order";
+					        	msg.id = "" + order.id;
+					        	msg.fields = gson.toJson(order);
+								Main.server.getBroadcastOperations().sendEvent("update", msg);
+							}else {
+								Order order = Main.orders.remove(0);
+								Message msg = new Message();
+					        	msg.action = "removed";
+					        	msg.type = "order";
+					        	msg.id = "" + order.id;
+					        	msg.fields = gson.toJson(order);
+								Main.server.getBroadcastOperations().sendEvent("update", msg);
+							}
+						}
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+			}
+		}).start();
+        
+        Thread.sleep(Integer.MAX_VALUE);
 
         server.stop();
     }
