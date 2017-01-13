dfd685f05503fd735d728f5353b87b479265e456
Felipe Jun Fujioka Shida <ffujioka@topster.com.br>
2017-01-13 18:36:56 -0200

    rest interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import static spark.Spark.*;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 import com.google.gson.Gson;
 
@@ -27,38 +25,16 @@ public class Main {
 		    return ((Order)obj).id.equals(this.id);
 		}
 	}
-	
-	public static class Message {
-		public String action;
-		public String type;
-		public String id;
-		public String fields;
-		public int seq = 0;
-		public int size;
-		
-		public Message() {
-			this.seq = ++currentSequenceNumber;
-			this.size = Main.orders.size();
-		}
-		
-	}
 
-	
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, URISyntaxException {
     	
     	orders = new ArrayList<Order>();
         
-        
-		try {
-			Main.ds = new DeepstreamClient( "localhost:6020" );
-			Main.ds.login();
-			System.out.println("deepstream login");
-			List list = Main.ds.record.getList("orders");
-			list.setEntries(new ArrayList<String>());
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		Main.ds = new DeepstreamClient( "localhost:6020" );
+		Main.ds.login();
+		System.out.println("deepstream login");
+		List list = Main.ds.record.getList("orders");
+		list.setEntries(new ArrayList<String>());
         
         
         options("/*", (request, response) -> {
@@ -95,11 +71,11 @@ public class Main {
         	Order body = gson.fromJson(req.body(), Order.class);
         	if(body.id != null && !body.id.equals("") && !orders.contains(body)){
 	        	orders.add(body);
-	        	Message msg = new Message();
-	        	msg.action = "added";
-	        	msg.type = "order";
-	        	msg.id = body.id;
-	        	msg.fields = gson.toJson(body);
+	        	Record record  = Main.ds.record.getRecord("orders/"+body.id);
+	        	record.set(body);
+	        	List local = Main.ds.record.getList("orders");
+	        	local.addEntry("orders/"+body.id);
+	        	
         	}
         	return "{}";
         });
@@ -107,13 +83,17 @@ public class Main {
         put("/orders/:id", (req, res) -> {
         	Gson gson = new Gson();
         	Order body = gson.fromJson(req.body(), Order.class);
-        	orders.remove(body);
-        	orders.add(body);
-        	Message msg = new Message();
-        	msg.action = "changed";
-        	msg.type = "order";
-        	msg.id = body.id;
-        	msg.fields = gson.toJson(body);
+        	if(body.id != null && !body.id.equals("") && orders.contains(body)){
+	        	orders.remove(body);
+	        	orders.add(body);
+	//        	Message msg = new Message();
+	//        	msg.action = "changed";
+	//        	msg.type = "order";
+	//        	msg.id = body.id;
+	//        	msg.fields = gson.toJson(body);
+	        	Record record  = Main.ds.record.getRecord("orders/"+body.id);
+	        	record.set(body);
+        	}
         	return "{}";
         });
         
@@ -122,10 +102,11 @@ public class Main {
         	obj.id = req.params(":id");
         	orders.remove(obj);
         	
-        	Message msg = new Message();
-        	msg.action = "removed";
-        	msg.type = "order";
-        	msg.id = req.params(":id");
+        	Record record  = Main.ds.record.getRecord("orders/"+obj.id);
+        	record.delete();
+        	List local = Main.ds.record.getList("orders");
+        	local.removeEntry("orders/"+obj.id);
+        	
         	return "deleted";
         });
         
@@ -161,41 +142,41 @@ public class Main {
 //			}
 //		}).start();
 
-        new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				while(true) {
-					try {
-						Thread.sleep(500);
-						synchronized(this) {
-							List list = Main.ds.record.getList("orders");
-							if (Main.orders.size() == 0 || Math.random() > 0.5){
-								Order order = new Order();
-								order.id = ""+((int)Math.floor(Math.random() * 1000));
-								order.name = "name from 2 " + order.id;
-								order.value = ""+((int)Math.floor((Math.random()*100)));
-								Main.orders.add(order);
-								Record record = Main.ds.record.getRecord("orders/"+order.id);
-								record.set("id", order.id);
-								record.set("name", order.name);
-								record.set("value", order.value);
-								list.addEntry("orders/"+order.id);
-							}else {
-								Order order = Main.orders.remove(0);
-								list.removeEntry("orders/"+order.id);
-							}
-						}
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				}
-			}
-		}).start();
+//        new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				while(true) {
+//					try {
+//						Thread.sleep(500);
+//						synchronized(this) {
+//							List list = Main.ds.record.getList("orders");
+//							if (Main.orders.size() == 0 || Math.random() > 0.5){
+//								Order order = new Order();
+//								order.id = ""+((int)Math.floor(Math.random() * 1000));
+//								order.name = "name from 2 " + order.id;
+//								order.value = ""+((int)Math.floor((Math.random()*100)));
+//								Main.orders.add(order);
+//								Record record = Main.ds.record.getRecord("orders/"+order.id);
+//								record.set("id", order.id);
+//								record.set("name", order.name);
+//								record.set("value", order.value);
+//								list.addEntry("orders/"+order.id);
+//							}else {
+//								Order order = Main.orders.remove(0);
+//								list.removeEntry("orders/"+order.id);
+//							}
+//						}
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					
+//				}
+//			}
+//		}).start();
         
-        Thread.sleep(Integer.MAX_VALUE);
+//        Thread.sleep(Integer.MAX_VALUE);
     }
 }
